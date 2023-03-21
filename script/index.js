12b5df6794ef9275955787572e5db9ea4894eaee
Swansky13 <lebedevdm13@yandex.ru>
2023-03-21 13:46:37 +0300

    итерация 3 попытка 1

--- script/index.js
+++ script/index.js
@@ -2,13 +2,18 @@ let openPopup = document.querySelector(".profile__info-edit");
 let popup = document.querySelector(".popup");
 let closePopup = popup.querySelector(".popup__button-close");
 let form = document.querySelector(".popup__form");
-let nameInput = form.querySelector(".popup__form-item_name");
-let jobInput = form.querySelector(".popup__form-item_subheading");
+let nameInput = form.querySelector(".popup__form_type_name");
+let jobInput = form.querySelector(".popup__form_type_subheading");
 let profileName = document.querySelector(".profile__info-name");
 let profileJob = document.querySelector(".profile__info-job");
 
 function togglePopup() {
   popup.classList.toggle("popup_opened");
+  
+  if (!popup.classList.contains("popup_opened")) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
 }
 
 function handleFormSubmit(evt) {
@@ -20,13 +25,6 @@ function handleFormSubmit(evt) {
   togglePopup();
 }
 
-openPopup.addEventListener("click", function () {
-  if (!popup.classList.contains("popup_opened")) {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-  }
-
-  togglePopup();
-});
+openPopup.addEventListener("click", togglePopup);
 closePopup.addEventListener("click", togglePopup);
 form.addEventListener("submit", handleFormSubmit);
